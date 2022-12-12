d64d01ebeb45397aae42765d128c71abf9114988
MrTibebu <abenezerhailutibebuasfaw@gmail.com>
2022-12-12 15:32:02 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,18 +2,14 @@
 
 /**
  *
- *  * main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
+ * main - Prints out a string to stdout.
+ * but the outcome is a piece of art, 
+ * Return: 0 if exited properly, non-zero otherwise
+ * /
 
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 			return (0);
 
 }
