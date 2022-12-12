750d74dcbb95f698f22c8deb3a2e7b7cb9bd54a0
MrTibebu <abenezerhailutibebuasfaw@gmail.com>
2022-12-12 15:55:32 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
- *
  * main - Prints out a string to stdout.
  * but the outcome is a piece of art, 
  * Return: 0 if exited properly, non-zero otherwise
- * /
+ */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
-
+	return (0);
 }
