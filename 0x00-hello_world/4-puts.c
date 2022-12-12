ab02dcaf98d79fb9693e71ee9444c260f118e417
MrTibebu <abenezerhailutibebuasfaw@gmail.com>
2022-12-12 15:26:52 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *
  *  * Description: main - Prints out a string to stdout.
@@ -6,8 +7,13 @@
  *   * Return: 0 if success.
  *
  *    */
+
 int main(void)
+
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
+
 			return (0);
+
 }
