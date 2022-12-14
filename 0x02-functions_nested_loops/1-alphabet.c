6cd7711af48e1a3981a38c8f4d8e6c8893cc78e5
MrTibebu <abenezerhailutibebuasfaw@gmail.com>
2022-12-13 17:40:03 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
- *print_alphabet - abcd
+ *print_alphabet: - abcd
  *Description: print lowercase alphabet
  *Return: void.
  */
 void print_alphabet(void)
 {
 	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+
 	int i;
+
 	for (i = 0; i < 26; i++)
 	{
 		_putchar(alphabet[i]);
