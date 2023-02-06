a600c9dda743f66903cca2c384ba8b40d6c77e65
MrTibebu <abenezerhailutibebuasfaw@gmail.com>
2023-02-06 15:12:12 -0800

    0x15. C - File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,6 +10,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int nletters;
 	int rwr;
+
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
